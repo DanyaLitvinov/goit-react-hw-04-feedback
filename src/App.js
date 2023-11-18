27e9a360ba2b404cbab837961642636da017ce38
DanyaLitvinov <vfvdaniel07@gmail.com>
2023-11-18 18:17:33 +0200

    123

--- src/App.js
+++ src/App.js
@@ -1,59 +1,54 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import FeedbackOptions from './Feedback/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 import Notification from './Notification/Notification';
 
-class App extends Component {
-  state = {
+function App() {
+  const [feedback, setFeedback] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
-  };
+  });
 
-  onLeaveFeedback = (e) => {
+  const onLeaveFeedback = (e) => {
     const currentBtnValue = e.currentTarget.value;
-    this.setState((prevState) => ({
-      ...prevState,
-      [currentBtnValue]: prevState[currentBtnValue] + 1,
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [currentBtnValue]: prevFeedback[currentBtnValue] + 1,
     }));
   };
 
-  countTotalFeedback = () => {
-    const valuesArr = Object.values(this.state);
+  const countTotalFeedback = () => {
+    const valuesArr = Object.values(feedback);
     return valuesArr.reduce((acc, val) => {
       acc += val;
       return acc;
     });
   };
 
-  countPositiveFeedbackPercentage = () => {
-    return Math.trunc((this.state.good / this.countTotalFeedback()) * 100);
+  const countPositiveFeedbackPercentage = () => {
+    return Math.trunc((feedback.good / countTotalFeedback()) * 100);
   };
 
-  render() {
-    const btnNames = Object.keys(this.state);
-    const totalFeedback = this.countTotalFeedback();
+  const btnNames = Object.keys(feedback);
+  const totalFeedback = countTotalFeedback();
 
-    return (
-      <div className="App">
-        <section>
-          <FeedbackOptions
-            options={btnNames}
-            onLeaveFeedback={this.onLeaveFeedback}
+  return (
+    <div className="App">
+      <section>
+        <FeedbackOptions options={btnNames} onLeaveFeedback={onLeaveFeedback} />
+        {totalFeedback > 0 ? (
+          <Statistics
+            stat={feedback}
+            total={totalFeedback}
+            positivePercentage={countPositiveFeedbackPercentage()}
           />
-          {totalFeedback > 0 ? (
-            <Statistics
-              stat={this.state}
-              total={totalFeedback}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          ) : (
-            <Notification message="No feedback given" />
-          )}
-        </section>
-      </div>
-    );
-  }
+        ) : (
+          <Notification message="No feedback given" />
+        )}
+      </section>
+    </div>
+  );
 }
 
 export default App;
